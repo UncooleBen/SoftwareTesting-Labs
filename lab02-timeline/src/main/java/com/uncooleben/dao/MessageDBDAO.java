ae2d27fe910fb787ee6b8ff23c399cd0d4e82e32
uncooleben <pengjuntao2016@foxmail.com>
2019-10-25 23:40:04 +0800

    Tested MessageDBDAO

--- lab02-timeline/src/main/java/com/uncooleben/dao/MessageDBDAO.java
+++ lab02-timeline/src/main/java/com/uncooleben/dao/MessageDBDAO.java
@@ -1,21 +1,18 @@
 package com.uncooleben.dao;
 
 public class MessageDBDAO {
-  private static String platform="1";  //0 stands for MySQL, 1 stands for SQLServer
-  private MessageDAO actualDAO;
+	private static String platform = "0"; // 0 stands for MySQL, 1 stands for SQLServer
+	private MessageDAO actualDAO;
 
-  public MessageDAO getActualDAO() {
-    return actualDAO;
-  }
+	public MessageDAO getActualDAO() {
+		return actualDAO;
+	}
 
-  public MessageDBDAO(){
-    if (platform.equals("0"))
-    {
-      actualDAO=new MessageMySQLDAO();
-    }
-    else if(platform.equals("1"))
-    {
-      actualDAO=new MessageSQLServerDAO();
-    }
-  }
+	public MessageDBDAO() {
+		if (platform.equals("0")) {
+			actualDAO = new MessageMySQLDAO();
+		} else if (platform.equals("1")) {
+			actualDAO = new MessageSQLServerDAO();
+		}
+	}
 }
